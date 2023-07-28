14238ae3a1ad9881e3a011310446f8b63ae5743c
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-07-28 14:38:56 +0200

    Hero_section

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 
-export default function RootLayout({ children,}: {
+export default function RootLayout({ children}: {
   children: React.ReactNode
 }) {
   return ( 
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,48 +1,115 @@
 import styled from "styled-components";
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Hero = () => {
-  return (
+  return ( 
     <HeroContainer>
+        <TextContainer>
+          <WelcomeText>Welcome</WelcomeText>
+          <ToText> ____ to your new home</ToText>
 
-      <ImageContainer>
-          <StyledImage src="../../../public/assets/headerImage.jpg" alt="Image"/>
-      </ImageContainer>
+            <SearchBarContainer>
+              <Input type="text" placeholder="Search by location" />
+              <Select>
+                <option value="">Property Type</option>
+                <option value="house">House</option>
+                <option value="apartment">Apartment</option>
+                <option value="condo">Condo</option>
+              </Select>
+              <Input type="number" placeholder="Price" />
+              <Button>
+                <StyledSearchIcon/>
+              </Button>
 
+        </SearchBarContainer>
+        </TextContainer>
+    
+      <ImageContainer>      
+          <StyledImage src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="image"/>    
+      </ImageContainer>  
+      
     </HeroContainer>
   )
 }
 
 const HeroContainer = styled.div`
-  width: 100%;
-  height: 500px; 
   position: relative;
+  display: flex;
+  width: 100%;
+  height: 500px;
   top: 0;
-  left: 0;
-  background-color: #00001a;
-  z-index: 100;
+  background-color: #030316;
+  
+`;
+const TextContainer = styled.div`
+  flex-direction: column;
+  margin-left: 150px;
+  width: 50%;
+  border: 1px solid white;
+`;
+const WelcomeText = styled.p`
+  color: white;
+  font-size: 65px;
+  text-align: center;
+`;
+
+const ToText = styled.p`
+  color: white;
+  font-size: 45px;
+  text-align: center;
+`;
+
+
+const SearchBarContainer = styled.div` 
+  display: flex;
+  width: 80%;
+  margin-left: 30px;
+  align-items: center;
+  border: 1px solid white;
+  justify-content: space-between;
+  padding: 2px;
+`;
+
+const Input = styled.p`
+    margin-right: 10px;
+`;
+
+const Select = styled.select`
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  padding: 1px 2px;
+  background-color: #eca710;
+  color: white;
+  border: none;
+ 
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f3a30f;
+  }
+`;
+const StyledSearchIcon = styled(SearchIcon)`
+  color: black;
+  font-size: 30px;
 `;
 
 const ImageContainer = styled.div`
-  width: 300px;
-  height: 200px;
-  position: relative;
-  border: 1px solid white;
-  top: 100px;
-  left: 0px;
+  width: 33%;
+  height: 80%;
+  margin-left: auto;
+  margin-top: 45px;
+  overflow: hidden;
 `;
 
-const StyledImage = styled.img`
-  position: absolute;
-  top: 0;
-  right: 0;
+const StyledImage = styled.img`  
   width: 100%;
-  height: 100%;
-  
+  height: 100%;  
+  object-fit: cover;
 `;
-
-
-
-
-
-
 export default Hero
